0addcea966cd6f09807d54be5071567c00c3e1fa
Antu <122485403+antukung@users.noreply.github.com>
2024-01-28 01:20:07 -0300

    Add files via upload

--- algoritmoburbuja.js
+++ algoritmoburbuja.js
@@ -1,15 +1,21 @@
 var lista = [];
 
-let valor1 = prompt ("Dame el primer valor");
-let valor2 = prompt ("Dame el segundo valor");
-let valor3 = prompt ("Dame el tercer valor");
-let valor4 = prompt ("Dame el cuarto valor");
-let valor5 = prompt ("Dame el quinto valor");
-let valor6 = prompt ("Dame el sexto valor");
-let valor7 = prompt ("Dame el septimo valor");
-let valor8 = prompt ("Dame el octavo valor");
-let valor9 = prompt ("Dame el noveno valor");
-let valor10 = prompt ("Dame el decimo valor");
+const texto= document.querySelector("#texto");
+
+
+function Burbuja() {
+
+    
+const valor1= document.querySelector("#numero-1").value;
+const valor2= document.querySelector("#numero-2").value;
+const valor3= document.querySelector("#numero-3").value;
+const valor4= document.querySelector("#numero-4").value;
+const valor5= document.querySelector("#numero-5").value;
+const valor6= document.querySelector("#numero-6").value;
+const valor7= document.querySelector("#numero-7").value;
+const valor8= document.querySelector("#numero-8").value;
+const valor9= document.querySelector("#numero-9").value;
+const valor10= document.querySelector("#numero-10").value;
 
 lista.push(parseInt(valor1));
 lista.push(parseInt(valor2));
@@ -21,10 +27,6 @@ lista.push(parseInt(valor7));
 lista.push(parseInt(valor8));
 lista.push(parseInt(valor9));
 lista.push(parseInt(valor10));
-
-Burbuja();
-
-function Burbuja() {
     
     var n, i, k, aux;
     n=lista.length;
@@ -41,5 +43,8 @@ function Burbuja() {
         
     }
     
-    alert(lista);
+    texto.innerHTML===" ";
+    texto.innerHTML= lista.join(", ");
+
+
 }
